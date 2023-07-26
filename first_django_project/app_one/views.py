21684a3006d5fa9b0c66869f99cf802cf09f6517
Ghada Sassi <sassighada5@gmail.com>
2023-07-26 10:40:48 +0200

    first assignment

---
+++ first_django_project/app_one/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import HttpResponse, redirect 
+from django.http import JsonResponse
+
+# Create your views here.
+
+def root(request):
+    return redirect("/blogs")
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return redirect("/blogs")
+
+def bonus(request):
+    return JsonResponse({"title": "my first blog", "content": "some text here"})
